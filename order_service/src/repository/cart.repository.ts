b0eeeb2406ae2a7fef90f6b4c1f6f7cdb01b2cee
= <=>
2024-04-14 22:34:45 +0900

    add drizzle

--- order_service/src/repository/cart.repository.ts
+++ order_service/src/repository/cart.repository.ts
@@ -1,25 +1,37 @@
-import { CartRepositoryType } from "../types/repository.type";
+import { DB } from '../db/db.connection';
+import { carts } from '../db/schema';
+import { CartRepositoryType } from '../types/repository.type';
 
-const db = {}
+const db = {};
 
 const createCart = async (input: any): Promise<{}> => {
-    // connect to db
-    // perform db operations
-    return Promise.resolve({message: "fake response from cart repository", input})
-}
+  // connect to db
+  const result = await DB.insert(carts)
+    .values({
+      customerId: 123,
+    })
+    .returning({ cartId: carts.id });
+
+  console.log(result);
+  // perform db operations
+  return Promise.resolve({
+    message: 'fake response from cart repository',
+    input,
+  });
+};
 const findCart = async (input: any): Promise<{}> => {
-    return Promise.resolve({})
-}
+  return Promise.resolve({});
+};
 const updateCart = async (input: any): Promise<{}> => {
-    return Promise.resolve({})
-}
+  return Promise.resolve({});
+};
 const deleteCart = async (input: any): Promise<{}> => {
-    return Promise.resolve({})
-}
+  return Promise.resolve({});
+};
 
 export const CartRepository: CartRepositoryType = {
-    create: createCart,
-    find: findCart,
-    update: updateCart,
-    delete: deleteCart,
-}
+  create: createCart,
+  find: findCart,
+  update: updateCart,
+  delete: deleteCart,
+};
